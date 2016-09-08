4f0def59e91c9e98c77f042a0724ea96c1702b5c
Muhammad Vaid <molvy32@gmail.com>
2016-09-08 05:33:48 +0200

    Implementation of multiple face detection with image preprocessing (converted image to grayscale, shrunk image and equalized histogram)
    Original image is outputted with overlayed rectangles indicating the locations of detected faces

--- AutomatedAttendance/AutomatedAttendance/main.cpp
+++ AutomatedAttendance/AutomatedAttendance/main.cpp
@@ -9,171 +9,140 @@ using namespace std;
 using namespace cv;
 
 // Function Headers
-void detectAndDisplay(Mat frame);
+void detectAndDisplay(Mat img);
 
-// Global variables
+// Global variables	
+
 // Copy this file from opencv/data/haarscascades to target folder
-string face_cascade_name = "c:/haarcascade_frontalface_alt.xml";
-CascadeClassifier face_cascade;
-string window_name = "Capture - Face detection";
-int filenumber; // Number of file to be saved
-string filename;
+string face_cascade_name = "c:/opencv-build/install/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml";
+CascadeClassifier faceDetector;
+
+//int filenumber; // Number of file to be saved
+//string filename;
+
 
 // Function main
 int main(void)
 {
-	VideoCapture capture(0);
-	IplImage* img = cvLoadImage("united1.jpg");
-	
-	if (!capture.isOpened())  // check if we succeeded
-		return -1;
-
-	// Load the cascade
-	if (!face_cascade.load(face_cascade_name))
-	{
-		printf("--(!)Error loading\n");
-		return (-1);
-	};
-
-	// Read the video stream
-	Mat frame(img);
-
-	for (;;)
-	{
-		//capture >> frame;
-
-		// Apply the classifier to the frame
-		if (!frame.empty())
-		{
-			detectAndDisplay(frame);
-		}
-		else
-		{
-			printf(" --(!) No captured frame -- Break!");
-			break;
-		}
-
-		int c = waitKey(10);
-
-		if (27 == char(c))
-		{
-			break;
-		}
+	//Load detector
+	try {
+		faceDetector.load(face_cascade_name);
 	}
-
+	catch (cv::Exception e) {}
+	if (faceDetector.empty()) {
+		cerr << "ERROR: Couldn't load Face Detector (";
+		cerr << face_cascade_name << ")!" << endl;
+		exit(1);
+	}
+	
+	//Load image and call detect class
+	IplImage* pic = cvLoadImage("test1.jpg");
+	Mat img(pic);
+	if (!img.empty())
+	{
+		detectAndDisplay(img);
+	}
+	else
+	{
+		printf("(!)-- No captured frame --(!)\n\n"); 
+	}
+	
+	system("pause");
 	return 0;
 }
 
-// Function detectAndDisplay
-void detectAndDisplay(Mat frame)
+
+void detectAndDisplay(Mat img)
 {
-	std::vector<Rect> faces;
-	Mat frame_gray;
-	Mat crop;
-	Mat res;
-	Mat gray;
-	string text;
-	stringstream sstm;
+	Mat modified;		//Gets altered with each change
+	Mat gray;			//Store greyscale image
+	Mat smallImg;		//Store resized image
+	Mat equalizedImg;	//Store equalized image
 
-	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
+	//CONVERT TO GRAYSCALE
+	if (img.channels() == 3) {
+		cvtColor(img, gray, CV_BGR2GRAY);
+	}
+	else if (img.channels() == 4) {
+		cvtColor(img, gray, CV_BGRA2GRAY);
+	}
+	else {
+		// Access the grayscale input image directly.
+		gray = img;
+	}
+	modified = gray;
 
-	// Detect faces
-	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+	//SHRIK IMAGE FOR FASTER PROCESSING
+	const int DETECTION_WIDTH = 800;
 	
-	// Set Region of Interest
-	cv::Rect roi_b;
-	cv::Rect roi_c;
+	float scale = modified.cols / (float)DETECTION_WIDTH;
+	if (modified.cols > DETECTION_WIDTH) {
+		// Shrink the image while keeping the same aspect ratio.
+		int scaledHeight = cvRound(modified.rows / scale);
+		resize(modified, smallImg, Size(DETECTION_WIDTH, scaledHeight));
+	}
+	else {
+		// Access the input directly since it is already small.
+		smallImg = modified;
+	}
+	modified = smallImg;
 
-	size_t ic = 0; // ic is index of current element
-	int ac = 0; // ac is area of current element
 
-	size_t ib = 0; // ib is index of biggest element
-	int ab = 0; // ab is area of biggest element
+	//EQUALIZE HISTOGRAMS - IMPROVES CONTRAST AND BRIGHTNESS
+	equalizeHist(modified, equalizedImg);
+	modified = equalizedImg;
 
-	for (ic = 0; ic < faces.size(); ic++) // Iterate through all current elements (detected faces)
 
+	//Parameters for detectMulti
+	int flags = CASCADE_SCALE_IMAGE; // Search for many faces.
+	Size minFeatureSize(20, 20); // Smallest face size.
+	float searchScaleFactor = 1.1f; // How many sizes to search.
+	int minNeighbors = 4; // Reliability vs many faces.
+						  
+	// Detect objects in the small grayscale image.
+	//Detect faces and stores rectangles in vector
+	vector<Rect> faces;
+	faceDetector.detectMultiScale(modified, faces, searchScaleFactor, minNeighbors, flags, minFeatureSize);
+	Rect roi_b;
+
+	//How many faces did multiscale detect - [test parameter]
+	printf("%d faces were detected\n\n", faces.size());
+
+	// Enlarge the results if the image was temporarily shrunk.
+	if (img.cols > DETECTION_WIDTH)
 	{
-		roi_c.x = faces[ic].x;
-		roi_c.y = faces[ic].y;
-		roi_c.width = (faces[ic].width);
-		roi_c.height = (faces[ic].height);
-
-		ac = roi_c.width * roi_c.height; // Get the area of current element (detected face)
-
-		roi_b.x = faces[ib].x;
-		roi_b.y = faces[ib].y;
-		roi_b.width = (faces[ib].width);
-		roi_b.height = (faces[ib].height);
-
-		ab = roi_b.width * roi_b.height; // Get the area of biggest element, at beginning it is same as "current" element
-
-		//if (ac > ab)							//REMOVED THIS TO SAVE ALL FACES
-		//{
-			ib = ic;
-			roi_b.x = faces[ib].x;
-			roi_b.y = faces[ib].y;
-			roi_b.width = (faces[ib].width);
-			roi_b.height = (faces[ib].height);
-		//}
-
-		crop = frame(roi_b);
-		resize(crop, res, Size(128, 128), 0, 0, INTER_LINEAR); // This will be needed later while saving images
-		cvtColor(crop, gray, CV_BGR2GRAY); // Convert cropped image to Grayscale
-
-										   // Form a filename
-		filename = "";
-		stringstream ssfn;
-		ssfn << filenumber << ".png";
-		filename = ssfn.str();
-		filenumber++;
-
-		imwrite(filename, gray);
-
-		Point pt1(faces[ic].x, faces[ic].y); // Display detected faces on main window - live stream from camera
-		Point pt2((faces[ic].x + faces[ic].height), (faces[ic].y + faces[ic].width));
-		rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
+		for (int i = 0; i < (int)faces.size(); i++) {
+			faces[i].x = cvRound(faces[i].x * scale);
+			faces[i].y = cvRound(faces[i].y * scale);
+			faces[i].width = cvRound(faces[i].width * scale);
+			faces[i].height = cvRound(faces[i].height * scale);
+		}
+	}
+	// If the object is on a border, keep it in the image.
+	for (int i = 0; i < (int)faces.size(); i++)
+	{
+		if (faces[i].x < 0)
+			faces[i].x = 0;
+		if (faces[i].y < 0)
+			faces[i].y = 0;
+		if (faces[i].x + faces[i].width > img.cols)
+			faces[i].x = img.cols - faces[i].width;
+		if (faces[i].y + faces[i].height > img.rows)
+			faces[i].y = img.rows - faces[i].height;
 	}
 
-	// Show image
-	sstm << "Crop area size: " << roi_b.width << "x" << roi_b.height << " Filename: " << filename;
-	text = sstm.str();
-
-	putText(frame, text, cvPoint(30, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0, 0, 255), 1, CV_AA);
-	imshow("original", frame);
-
-	if (!crop.empty())
+	//ADD RECTANGLES TO ORIGINAL IMAGE
+	for (int i = 0; i < (int)faces.size(); i++)
 	{
-		imshow("detected", crop);
+		Point pt1(faces[i].x, faces[i].y); // Display detected faces on main window - live stream from camera
+		Point pt2((faces[i].x + faces[i].height), (faces[i].y + faces[i].width));
+		rectangle(img, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
 	}
-	else
-		destroyWindow("detected");
+
+	//OUTPUT ORIGINAL PICTURE WITH RECTANGLES AROUND EACH FACE
+	imshow("Detected", img);
+	waitKey(0);
+	destroyWindow("Detected");
+
 }
-//
-//void Detect(){
-//
-//
-//	//STEP 3-SET THE SOURCE OF IMAGE
-//	//fetch the frame captured by web cam
-//	Image<Bgr, Byte> ImageFrame = capture.QueryFrame();
-//
-////FACE DETECTION PROCESS
-//if (ImageFrame != null) // confirm that image is valid
-//{
-//	//STEP 4.1-convert the image to gray scale
-//	Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
-//
-//	//STEP 4.2-Detect faces from the gray-scale image and store into an array of type 'var',i.e 'MCvAvgComp[]'
-//	var faces = grayframe.DetectHaarCascade(haar, 1.4, 4,
-//		HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
-//		new Size(25, 25))[0];
-//
-//	//STEP 4.3-draw a green rectangle on each detected face in image
-//	foreach(var face in faces)
-//	{
-//		ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
-//	}
-//}
-////STEP 4.4-Display the image
-//CamImageBox.Image = ImageFrame; //CamImageBox is an EmguCV imageBox
-//}
